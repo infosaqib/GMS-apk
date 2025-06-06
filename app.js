0ad3915b2a8d5abd99c8eb66535183717c5da8dc
infosaqib <hafizsaqi555@gmail.com>
2025-06-06 12:25:12 +0500

    getslim changes

--- app.js
+++ app.js
@@ -5,22 +5,7 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 require('dotenv').config();
 
-// In app.js, before mounting routes
-// app.use((req, res, next) => {
-//   console.log('Incoming request:', {
-//       method: req.method,
-//       url: req.originalUrl,
-//       body: req.body
-//   });
-//   next();
-// });
-
 let indexRouter = require('./routes/index.route');
-const productRouter = require('./routes/product.route')
-let clientInvoicesRouter = require('./routes/client-invoice.route');
-let vendorInvoicesRouter = require('./routes/vendor-invoice.route');
-let clientRouter = require('./routes/client.route');
-let vendorRouter = require('./routes/vendor.route');
 let weightRecordRouter = require('./routes/weightRecord.route');
 let caloriesRecordRouter = require('./routes/caloriesRecord.route');
 let mealRecordRouter = require('./routes/mealRecord.route');
@@ -28,22 +13,15 @@ let mealRecordRouter = require('./routes/mealRecord.route');
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'src')));
+
 
 app.use('/', indexRouter);
-app.use('/api/client-invoices', clientInvoicesRouter);
-app.use('/api/vendor-invoices', vendorInvoicesRouter);
-app.use('/api/products', productRouter)
-app.use('/api/clients', clientRouter)
-app.use('/api/vendors', vendorRouter)
 app.use('/api/calories',caloriesRecordRouter)
 app.use('/api/weight', weightRecordRouter)
 app.use('/api/meal', mealRecordRouter)
@@ -54,17 +32,5 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- routes/index.route.js
+++ routes/index.route.js
@@ -7,118 +7,12 @@ const bodyParser = require('body-parser')
 
 //* Database Connection
 const connectDB = require('../db');
-const clientInvoiceModel = require('../models/client-invoice.model');
-const vendorInvoiceModel = require('../models/vendor-invoice.model');
-const clientModel = require('../models/client.model');
-const vendorModel = require('../models/vendor.model');
-const productModel = require('../models/product.model');
 connectDB();
 
 
 //* Routes
 router.get('/', async function (req, res) {
-  try {
-    // Run all queries concurrently to improve performance
-    const [
-      clients,
-      vendors,
-      products,
-      totalSalesValue,
-      totalSales,
-      totalPurchasesValue,
-      totalPurchases
-    ] = await Promise.all([
-      clientModel.countDocuments(),
-      vendorModel.countDocuments(),
-      productModel.countDocuments(),
-      clientInvoiceModel.aggregate([
-        {
-          $group: {
-            _id: null,
-            total: { $sum: '$total_price' }
-          }
-        }
-      ]),
-      clientInvoiceModel.countDocuments(),
-      vendorInvoiceModel.aggregate([
-        {
-          $group: {
-            _id: null,
-            total: { $sum: '$totalPrice' }
-          }
-        }
-      ]),
-      vendorInvoiceModel.countDocuments()
-    ]);
-
-    // Extract totals or set default value
-    const totalProfit = totalSalesValue[0]?.total || 0;
-    const totalSpending = totalPurchasesValue[0]?.total || 0;
-
-    // Render the view
-    return res.render('index', {
-      clients,
-      vendors,
-      products,
-      totalProfit,
-      totalSales,
-      totalSpending,
-      totalPurchases
-    });
-  } catch (error) {
-    // Error handling
-    console.error('Error occurred:', error);
-  return res.status(500).send('Something went wrong. Please try again later.');
-  }
-});
-
-
-router.get('/invoices', async function (req, res) {
-  try {
-    // Fetch clients and vendors
-    const clientInvoices = await clientInvoiceModel.find();
-    const vendorInvoices = await vendorInvoiceModel.find();
-  return res.render("invoice", { clientInvoices, vendorInvoices });
-
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Internal Server Error");
-  }
-});
-router.get('/products', function (req, res) {
-  return res.render('product');
-});
-router.get('/tracking', function (req, res) {
-  return res.render('tracking');
-});
-
-router.get("/clients", async function (req, res) {
-  try {
-    const clients = await clientModel.find(); // Fetch all users
-    return res.render("client", { clients });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Internal Server Error");
-  }
-});
-
-router.get('/clients/client-profile', function (req, res) {
-  return res.render('clientProfile');
-});
-router.get('/vendors', async function (req, res) {
-  try {
-    const vendors = await vendorModel.find(); // Fetch all users
-    return res.render("vendor", { vendors });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Internal Server Error");
-  }
-});
-router.get('/vendors/vendor-profile', function (req, res) {
-  return res.render('vendorProfile');
-});
-router.get('/scan', function (req, res) {
-  return res.render('qr_scanner');
+res.send("hello world")
 });
 
 
@@ -126,8 +20,5 @@ router.get('/scan', function (req, res) {
 router.use(bodyParser.json())
 router.use(express.json())
 router.use(express.urlencoded({ extended: false }))
-
-
-
 
 module.exports = router;
